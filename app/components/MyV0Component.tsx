70ebc1ad85ba3c8a382e05f1d5dac8d57f00c479
lizavercelli <150980287+lizavercelli@users.noreply.github.com>
2023-11-15 17:45:26 -0800

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,62 +1,38 @@
-// you can overwrite this entire file with your v0 Component.
-// just copy and paste the "React" output over the entire file.
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/lnwtsAZRd7b
+ */
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
-import Link from "next/link";
-import React from "react";
-import {V0Logo} from "./symbols";
-
-function TestComponent() {
+export default function Component() {
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        justifyContent: "space-between",
-        flexDirection: "column",
-        width: "100%",
-      }}
-    >
-      <h3
-        style={{
-          maxWidth: "11em",
-        }}
-      >
-        This would be a pretty good place for a{" "}
-        <Link href="https://v0.dev/" target="_blank" rel="noopener noreferrer">
-          v0 component
-        </Link>
-        , wouldn't it?
-      </h3>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "flex-end",
-          width: "100%",
-        }}
-      >
-        <p
-          style={{
-            maxWidth: "20em",
-          }}
+    <div className="mx-auto max-w-md space-y-6 bg-blue-200 dark:bg-blue-800 rounded-md p-4">
+      <div className="space-y-2 text-center">
+        <h1 className="text-3xl font-bold text-green-700 dark:text-green-300">Newsletter Sign Up</h1>
+        <p className="text-blue-700 dark:text-blue-300">Sign up to receive updates and news.</p>
+      </div>
+      <div className="space-y-4">
+        <div className="space-y-2">
+          <Label className="text-purple-700 dark:text-purple-300" htmlFor="name">
+            Name
+          </Label>
+          <Input className="bg-white dark:bg-gray-700" id="name" placeholder="Enter your name" required />
+        </div>
+        <div className="space-y-2">
+          <Label className="text-purple-700 dark:text-purple-300" htmlFor="email">
+            Email
+          </Label>
+          <Input className="bg-white dark:bg-gray-700" id="email" placeholder="m@example.com" required type="email" />
+        </div>
+        <Button
+          className="w-full bg-yellow-500 hover:bg-yellow-600 dark:bg-yellow-300 dark:hover:bg-yellow-400"
+          type="submit"
         >
-          Go make one, then paste it into{" "}
-          <code
-            style={{
-              fontFamily: "var(--font-geist-mono)",
-              fontWeight: 500,
-              fontSize: "0.95em",
-              fontFeatureSettings: "'ss09'",
-            }}
-          >
-            app/components/MyV0Component.tsx
-          </code>
-        </p>
-
-        <V0Logo />
+          Subscribe
+        </Button>
       </div>
     </div>
-  );
+  )
 }
-
-export default TestComponent;
